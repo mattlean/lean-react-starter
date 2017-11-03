c7c861dbd285180ca2d9a557024297d92882b150
Matthew Lean <matt@mattlean.com>
2017-11-02 17:52:17 -0700

    Update Counter component to use Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import BSStarterTemplate from './BSStarterTemplate';
 import Counter from './Counter';
+import counter from '../reducers';
 import Error404 from './Error404';
 import HelloWorld from './HelloWorld';
 import MainNav from './MainNav';
 
+const store = createStore(counter);
+
 const App = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={MainNav} />
-      <Route exact path="/bootstrap_starter_template" component={BSStarterTemplate} />
-      <Route exact path="/counter" render={() => <Counter name="Counter Example" />} />
-      <Route exact path="/hello_world" component={HelloWorld} />
-      <Route component={Error404} />
-    </Switch>
+    <Provider store={store}>
+      <Switch>
+        <Route exact path="/" component={MainNav} />
+        <Route exact path="/bootstrap_starter_template" component={BSStarterTemplate} />
+        <Route exact path="/counter" render={() => <Counter name="Counter Example" />} />
+        <Route exact path="/hello_world" component={HelloWorld} />
+        <Route component={Error404} />
+      </Switch>
+    </Provider>
   </Router>
 );
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,49 +1,58 @@
 // @flow
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 type Props = {
-  name?: string
-};
-
-type State = {
+  onDecrement: Function,
+  onIncrement: Function,
+  name?: string,
   value: number
 };
 
-class Counter extends Component<Props, State> {
+class Counter extends Component<Props> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {
-      value: 0
-    };
-
-    this.increment = this.increment.bind(this);
+    // this.increment = this.increment.bind(this);
     // this.incrementAsync = this.incrementAsync.bind(this);
-    // this.incrementIfOdd = this.incrementIfOdd.bind(this);
-    this.decrement = this.decrement.bind(this);
+    this.incrementIfOdd = this.incrementIfOdd.bind(this);
+    // this.decrement = this.decrement.bind(this);
   }
 
-  increment() {
-    this.setState({ value: this.state.value + 1 });
-  }
+  incrementIfOdd: Function;
 
-  decrement() {
-    this.setState({ value: this.state.value - 1 });
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement();
+    }
   }
-
-  increment: Function;
-  decrement: Function;
 
   render() {
+    const { onDecrement, onIncrement, value } = this.props;
     return (
       <p>
         <b>{this.props.name}</b>
-        <br /> Clicked: {this.state.value} times <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
+        <br /> Clicked: {value} times<br />
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>
       </p>
     );
   }
 }
 
-export default Counter;
+const mapStateToProps = state => ({
+  value: state
+});
+const mapDispatchToProps = dispatch => ({
+  onDecrement() {
+    dispatch({ type: 'DECREMENT' });
+  },
+
+  onIncrement() {
+    dispatch({ type: 'INCREMENT' });
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
