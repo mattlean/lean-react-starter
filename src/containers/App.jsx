dd5fd4fd137f59402c381e6c038f2f9419572ed0
Matthew Lean <matt@mattlean.com>
2017-11-02 22:14:56 -0700

    Add Counter test, reclassify App & Counter as containers

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import BSStarterTemplate from './BSStarterTemplate';
+import BSStarterTemplate from '../components/BSStarterTemplate';
 import Counter from './Counter';
-import Error404 from './Error404';
-import HelloWorld from './HelloWorld';
-import Image from './Image';
-import MainNav from './MainNav';
+import Error404 from '../components/Error404';
+import HelloWorld from '../components/HelloWorld';
+import Image from '../components/Image';
+import MainNav from '../components/MainNav';
 import store from '../store';
 
 const App = () => (
