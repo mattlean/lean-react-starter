c610ab26a7209f340997a1d3c7fa78d6c0019800
Matthew Lean <matt@mattlean.com>
2017-11-10 12:32:13 -0800

    Update App container to match App component in non-Redux branch

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,6 +6,7 @@ import BSStarterTemplate from '../components/BSStarterTemplate';
 import Counter from './Counter';
 import Error404 from '../components/Error404';
 import HelloWorld from '../components/HelloWorld';
+import Image from '../components/Image';
 import MainNav from '../components/MainNav';
 import store from '../store';
 
@@ -17,6 +18,7 @@ const App = () => (
         <Route exact path="/bootstrap_starter_template" component={BSStarterTemplate} />
         <Route exact path="/counter" render={() => <Counter name="Counter Example" />} />
         <Route exact path="/hello_world" component={HelloWorld} />
+        <Route exact path="/image" component={Image} />
         <Route component={Error404} />
       </Switch>
     </Provider>
